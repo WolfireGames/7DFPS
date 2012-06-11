29f868a48af7ca5e239871faa172ab527cc2c402
David Rosen <david@wolfire.com>
2012-06-10 23:10:11 -0700

    Testing new rooms

---
+++ UnityProject/Assets/Standard Assets/Particles/Legacy Particles/TimedObjectDestructor.js
@@ -0,0 +1,15 @@
+var timeOut = 1.0;
+var detachChildren = false;
+
+function Awake ()
+{
+	Invoke ("DestroyNow", timeOut);
+}
+
+function DestroyNow ()
+{
+	if (detachChildren) {
+		transform.DetachChildren ();
+	}
+	DestroyObject (gameObject);
+}
