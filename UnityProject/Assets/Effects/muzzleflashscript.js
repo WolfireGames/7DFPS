d35604cb5539f1464fb00b0c1030e2e072a5425d
David Rosen <david@wolfire.com>
2012-06-11 18:10:25 -0700

    New muzzle flash

--- UnityProject/Assets/Effects/muzzleflashscript.js
+++ UnityProject/Assets/Effects/muzzleflashscript.js
@@ -1,17 +1,23 @@
 #pragma strict
 
+var opac = 0.0;
+
 function Start () {
-	var renderer = transform.GetComponentInChildren(MeshRenderer);
-	var color = renderer.material.GetColor("_TintColor");
-	var opac = Random.Range(0.0,1.0);
-	color.a = opac;
-	renderer.material.SetColor("_TintColor", color);
+	opac = Random.Range(0.0,1.0);
 	transform.localRotation.eulerAngles.z = Random.Range(0.0,360.0);
-	transform.localScale.x = Random.Range(0.4,1.0);
-	transform.localScale.y = Random.Range(0.4,1.0);
-	transform.localScale.z = Random.Range(0.4,1.0);
+	transform.localScale.x = Random.Range(0.8,2.0);
+	transform.localScale.y = Random.Range(0.8,2.0);
+	transform.localScale.z = Random.Range(0.8,2.0);
 }
 
-function FixedUpdate() {
-	Destroy(gameObject);
+function Update() {
+	var renderers = transform.GetComponentsInChildren(MeshRenderer);
+	var color = Vector4(opac,opac,opac,opac);
+	for(var renderer : MeshRenderer in renderers){
+		renderer.material.SetColor("_TintColor", color);
+	}
+	opac -= Time.deltaTime * 50.0;
+	if(opac <= 0.0){
+		Destroy(gameObject);
+	}
 }
