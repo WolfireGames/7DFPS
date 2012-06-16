4d79eb7b977d0990f8772027d370f109e22b7568
David Rosen <david@wolfire.com>
2012-06-15 19:01:06 -0700

    Update

--- UnityProject/Assets/Scripts/ShootableLight.js
+++ UnityProject/Assets/Scripts/ShootableLight.js
@@ -3,6 +3,9 @@
 var destroy_effect : GameObject;
 var light_color = Color(1,1,1);
 var destroyed = false;
+enum LightType {AIRPLANE_BLINK, NORMAL, FLICKER}
+public var light_type = LightType.NORMAL;
+private var blink_delay = 0.0;
 
 private var light_amount = 1.0;
 
@@ -22,6 +25,22 @@ function Start () {
 }
 
 function Update () {
+	if(!destroyed){
+		switch(light_type){
+			case LightType.AIRPLANE_BLINK:
+				if(blink_delay <= 0.0){
+					blink_delay = 1.0;
+					if(light_amount == 1.0){
+						light_amount = 0.0;
+					} else {
+						light_amount = 1.0;
+					}
+				}
+				blink_delay -= Time.deltaTime;
+				break;
+		}
+	}
+
 	var combined_color = Color(light_color.r * light_amount,light_color.g * light_amount,light_color.b * light_amount);
 	for(var light : Light in gameObject.GetComponentsInChildren(Light)){
 		light.color = combined_color;
