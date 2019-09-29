8a2abe89828c2231f23e73801f7542604d17a2a7
Kroporo <Michele.Butkus@gmx.de>
2019-09-29 11:31:44 +0200

    Added Modding support for 3 kinds of mods
    Supporting:
    - Guns (single gun per mod)
    - Level Tiles (multiple tiles per mod)
    - Custom (instantiates a gameobject and adds the "CustomScript" class provided by "scripts.bytes" [renamed scripts.dll])
    
    An Editorscript adds 3 buttons to the "Wolfire" menu inside Unity, allowing to export a folder to the specified mod type -> Exported to "Assets/Mods"
    
    When build, a Mods Folder will generate inside the data folder next to the Receiver.exe after starting the game for the first time.

---
+++ UnityProject/Assets/Editor/ModExport.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEditor;
+using System.Linq;
+
+public class ModExport : MonoBehaviour {
+    [MenuItem("Wolfire/Modding/Export Gun Mod")]
+    public static void ExportGun () {
+        ExportBundle(ModType.Gun);
+    }
+
+    [MenuItem("Wolfire/Modding/Export Level Tile Mod")]
+    public static void ExportLevelTile () {
+        ExportBundle(ModType.LevelTile);
+    }
+
+    [MenuItem("Wolfire/Modding/Export Custom Mod")]
+    public static void ExportCustom () {
+        ExportBundle(ModType.Custom);
+    }
+
+    public static void ExportBundle (ModType modType) {
+        var pathIn = EditorUtility.OpenFolderPanel("Select \"{modType.ToString()}\" Mod Folder", "Assets/", "");
+        var pathOut = $"Assets/Mods/{modType.ToString()}/modfile_{Path.GetFileName(pathIn)}";
+        
+        if(pathIn == "") // Happens when the user aborts path selection
+            return;
+        
+        Debug.Log($"Exporting \"{modType.ToString()}\" Mod: Source: \"{pathIn}\", Target: \"{pathOut}\"");
+
+        // Get Files and convert absolute paths to relative paths (required by the buildmap)
+        var absoluteFiles = Directory.GetFiles(pathIn).Where(name => !name.EndsWith(".meta") && !name.EndsWith(".cs")).ToArray();
+        var files = new string[absoluteFiles.Length];
+
+        if(files.Length > 0) {
+            var index = absoluteFiles[0].IndexOf("Assets");
+            for (int i = 0; i < files.Length; i++)
+                files[i] = absoluteFiles[i].Substring(index);
+        }
+
+        // Prepare Bundle
+        var buildMap = new AssetBundleBuild[1];
+        buildMap[0].assetBundleName = Path.GetFileName(pathIn);
+        buildMap[0].assetNames = files;
+
+        // Build Folder / Bundle
+        Directory.CreateDirectory(pathOut);
+        BuildPipeline.BuildAssetBundles(pathOut, buildMap, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
+
+        Debug.Log($"Export Completed. Name: \"{Path.GetFileName(pathIn)}\" with {files.Length} files");
+    }
+}
