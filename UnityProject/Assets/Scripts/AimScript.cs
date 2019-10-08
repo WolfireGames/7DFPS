42e8bae432340fc850a43982ec3036b411683e23
Kroporo <Michele.Butkus@gmx.de>
2019-10-08 17:29:30 +0200

    Added Cheat displays
    Also warning people if they used the slomo button, but didn't use the slomo cheat.

--- UnityProject/Assets/Scripts/AimScript.cs
+++ UnityProject/Assets/Scripts/AimScript.cs
@@ -320,6 +320,7 @@ public class AimScript:MonoBehaviour{
     int unplayed_tapes = 0;
     
     // Cheats
+	bool hasCheated = false;
     bool god_mode = false;
     bool slomo_mode = false;
     int iddqd_progress = 0;
@@ -327,6 +328,7 @@ public class AimScript:MonoBehaviour{
     int slomo_progress = 0;
     float cheat_delay = 0.0f;
     float level_reset_hold = 0.0f;
+    float slomoWarningDuration = 0f;
     
     // Inventory slots
     int target_weapon_slot = -2;
@@ -1008,16 +1010,19 @@ public class AimScript:MonoBehaviour{
     			}
     		}
     	}
-    	
+
     	if(character_input.GetButtonDown("Aim Toggle")){
     		aim_toggle = !aim_toggle;
     	}
-    	if(character_input.GetButtonDown("Slow Motion Toggle") && slomo_mode){
-    		if(Time.timeScale == 1.0f){
-    			Time.timeScale = 0.1f;
-    		} else {
-    			Time.timeScale = 1.0f;
-    		}
+    	if(character_input.GetButtonDown("Slow Motion Toggle")){
+    		if(slomo_mode) {
+				if(Time.timeScale == 1.0f)
+					Time.timeScale = 0.1f;
+				else
+					Time.timeScale = 1.0f;
+			} else {
+				slomoWarningDuration = 1f;
+			}
     	}
     }
     
@@ -1083,7 +1088,8 @@ public class AimScript:MonoBehaviour{
     		++iddqd_progress; cheat_delay = 1.0f;
     	} else if(iddqd_progress == 4 && Input.GetKeyDown("d")){
     		iddqd_progress = 0;
-    		god_mode = !god_mode; 
+    		god_mode = !god_mode;
+			hasCheated = true;
     		PlaySoundFromGroup(holder.sound_scream, 1.0f);
     	}
     	if(idkfa_progress == 0 && Input.GetKeyDown("i")){
@@ -1096,6 +1102,7 @@ public class AimScript:MonoBehaviour{
     		++idkfa_progress; cheat_delay = 1.0f;
     	} else if(idkfa_progress == 4 && Input.GetKeyDown("a")){
     		idkfa_progress = 0;
+			hasCheated = true;
     		if(loose_bullets.Count < 30){
     			PlaySoundFromGroup(sound_bullet_grab, 0.2f);
     		}
@@ -1115,6 +1122,7 @@ public class AimScript:MonoBehaviour{
     	} else if(slomo_progress == 4 && Input.GetKeyDown("o")){
     		slomo_progress = 0;
     		slomo_mode = true;
+			hasCheated = true;
     		if(Time.timeScale == 1.0f){
     			Time.timeScale = 0.1f;
     		} else {
@@ -1839,6 +1847,23 @@ public class AimScript:MonoBehaviour{
     				display_text.Add(new DisplayLine("Advanced help: Hold [ ? ]", false));
     			}
     		}
+
+			if(hasCheated) {
+				display_text.Add(new DisplayLine("", false));
+				display_text.Add(new DisplayLine("Cheats used", true));
+
+				if(god_mode)
+					display_text.Add(new DisplayLine("God Mode enabled", true));
+				
+				if(slomo_mode)
+					display_text.Add(new DisplayLine("Slomo Mode enabled", Time.timeScale == 0.1f));
+			}
+
+			if(slomoWarningDuration > 0) {
+				display_text.Add(new DisplayLine("Slomo button requires slomo cheat!", false));
+				slomoWarningDuration -= Time.deltaTime * 0.2f;
+			}
+
     		GUIStyle style = holder.gui_skin.label;
     		float width = Screen.width * 0.5f;
     		int offset = 0;
