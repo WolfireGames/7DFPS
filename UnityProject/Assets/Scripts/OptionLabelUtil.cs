fe4ad798a279383906d2fb8aa60c8af90c2a3eae
Kroporo <Michele.Butkus@gmx.de>
2019-10-13 16:44:29 +0200

    Slider Labels are now in the same object as the slider. Added Value indicator to sliders
    This decreases the amount of non settings gameobjects inside the options list. (Makes it easier to work with)

---
+++ UnityProject/Assets/Scripts/OptionLabelUtil.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionLabelUtil : MonoBehaviour {
+    private Text textBox;
+
+    void Awake() {
+        this.textBox = GetComponent<Text>();
+    }
+
+    public void ChangeValue(float value) {
+        textBox.text = $"{value:0.00}";
+    }
+}
