3e612487f1f751b83da0252dc369a51843249113
Kroporo <Michele.Butkus@gmx.de>
2019-12-12 15:24:36 +0100

    Reset Selected gun via the "reset to defaults" button

--- UnityProject/Assets/Scripts/optionsmenuscript.cs
+++ UnityProject/Assets/Scripts/optionsmenuscript.cs
@@ -141,6 +141,7 @@ public class optionsmenuscript:MonoBehaviour{
         PlayerPrefs.SetInt("lock_gun_to_center", 0);
         PlayerPrefs.SetInt("mouse_invert", 0);
         PlayerPrefs.SetInt("toggle_crouch", 1);
+        PlayerPrefs.SetInt("selected_gun_index", -1);
 
         PlayerPrefs.SetFloat("post_processing", 1f);
         PlayerPrefs.SetFloat("ambient_intensity", 0.44f);
