0d8edc99532afa69eeefc528fa4e8eaa4581abb7
Brian Lindenhof <brian@shadowbrain.net>
2020-02-06 14:55:08 +0100

    Fixed Lefthanded reload, added offset to interaction sphere to account for future controllers or something

--- UnityProject/Assets/VRInputBridge.cs
+++ UnityProject/Assets/VRInputBridge.cs
@@ -46,7 +46,7 @@ public class VRInputBridge : MonoBehaviour
                 }
                 if (hand == HandSide.Left) {
                     SlideBounds = SlideObject.bounds;
-                    if (SlideBounds.Contains(VRInputController.instance.LeftHand.transform.position) || MagOut) {
+                    if (SlideBounds.Contains(VRInputController.instance.LHandSphere.transform.position) || MagOut) {
                         return VRInputController.instance.ActionPressDown(hand);
                     }
                     else {
@@ -55,7 +55,7 @@ public class VRInputBridge : MonoBehaviour
                 }
                 else {
                     SlideBounds = SlideObject.bounds;
-                    if (SlideBounds.Contains(VRInputController.instance.RightHand.transform.position) || MagOut) {
+                    if (SlideBounds.Contains(VRInputController.instance.RHandSphere.transform.position) || MagOut) {
                         return VRInputController.instance.ActionPressDown(hand);
                     }
                     else {
@@ -79,7 +79,7 @@ public class VRInputBridge : MonoBehaviour
                         }
                         else if (aimScript_ref.gun_script.HasGunComponent(GunAspect.EXTERNAL_MAGAZINE)){//Magazine into gun insert, have to hold the mag under the gun.
                             Vector3 magInsertPos = aimScript_ref.gun_script.GetComponent<ExternalMagazineComponent>().point_mag_to_insert.position;
-                            if (MagOut && Vector3.Distance(VRInputController.instance.LeftHand.transform.position, magInsertPos) < 0.075f && VRInputController.instance.LeftHand.transform.position.y < magInsertPos.y) {
+                            if (MagOut && Vector3.Distance(VRInputController.instance.LHandSphere.transform.position, magInsertPos) < 0.075f && VRInputController.instance.LHandSphere.transform.position.y < magInsertPos.y) {
                                 MagOut = false;
                                 return true;
                             }
@@ -94,7 +94,7 @@ public class VRInputBridge : MonoBehaviour
                         }
                         else if (aimScript_ref.gun_script.HasGunComponent(GunAspect.EXTERNAL_MAGAZINE)) {
                             Vector3 magInsertPos = aimScript_ref.gun_script.GetComponent<ExternalMagazineComponent>().point_mag_to_insert.position;
-                            if (MagOut && Vector3.Distance(magInsertPos, VRInputController.instance.RightHand.transform.position) < 0.05f) {
+                            if (MagOut && Vector3.Distance(magInsertPos, VRInputController.instance.RHandSphere.transform.position) < 0.05f && VRInputController.instance.RHandSphere.transform.position.y < magInsertPos.y) {
                                 MagOut = false;
                                 return true;
                             }
