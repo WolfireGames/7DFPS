ac9e2a8485a2c3411a4986c9f5b48bacfa370282
Max Danielsson <max@autious.net>
2020-02-28 09:48:10 +0100

    Fix build scripts for new build system setup

--- UnityProject/BuildLinux32.bat
+++ UnityProject/BuildLinux32.bat
@@ -1,7 +1,7 @@
 del /f /Q /S Receiver_linux32.zip
 del /f /Q /S "Build\linux32"
 
-"C:\Program Files\Unity\Editor\Unity" -quit -batchmode -projectPath "%~dp0" -executeMethod ReceiverBuildScript.BuildLinux32 -logFile unitylog.txt
+"%UNITY%" -quit -batchmode -projectPath "%~dp0" -executeMethod ReceiverBuildScript.BuildLinux32 -logFile unitylog.txt
 
 if errorlevel 1 (
     echo Failure To Run
