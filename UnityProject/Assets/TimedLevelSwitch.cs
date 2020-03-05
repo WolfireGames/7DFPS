783193426b5dbddb5c1c788c68879873a40b6b7b
Brian Lindenhof <brian@shadowbrain.net>
2020-03-05 20:14:35 +0100

    VR splashscreen

---
+++ UnityProject/Assets/TimedLevelSwitch.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class TimedLevelSwitch : MonoBehaviour
+{
+    public Renderer[] renderersToFade;
+    public float fadeTime = 2f;
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(1f);
+        Color fadecol = new Color(1f, 1f, 1f, 1f);
+        while (fadeTime > -0.1f) {
+            fadeTime -= Time.deltaTime;
+            fadecol.a -= Time.deltaTime / 2f;
+            renderersToFade[0].transform.localScale *= 1f + (Time.deltaTime / 10f);
+            renderersToFade[1].transform.localScale *= 1f + (Time.deltaTime / 10f);
+            renderersToFade[0].material.color = fadecol;
+            renderersToFade[1].material.color = fadecol;
+            yield return null;
+        }
+
+        SceneManager.LoadScene("scene");
+    }
+
+    void Update()
+    {
+        
+    }
+}
