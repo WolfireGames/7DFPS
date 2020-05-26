203efdc0a7fc9a1f0132f97999b31897dce7085f
Tuomas Närväinen <tuomas.narvainen@alternativegames.net>
2020-05-26 15:23:12 +0300

    Add SteamScript placeholder.

---
+++ UnityProject/Assets/Scripts/SteamScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Steamworks;
+using ImGuiNET;
+
+public class SteamScript : MonoBehaviour
+{
+    string steamName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (SteamManager.Initialized) {
+            steamName = SteamFriends.GetPersonaName();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ImGui.Begin("Steam Test Window");
+        ImGui.Text("User Steam Name: " + steamName);
+        ImGui.End();
+    }
+}
