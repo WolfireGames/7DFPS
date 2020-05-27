7723c54c10a58d98e48edbc38ba12b3df9edb398
Tuomas Närväinen <tuomas.narvainen@alternativegames.net>
2020-05-27 15:28:40 +0300

    List available mods in Steam window.

--- UnityProject/Assets/Scripts/SteamScript.cs
+++ UnityProject/Assets/Scripts/SteamScript.cs
@@ -20,7 +20,19 @@ public class SteamScript : MonoBehaviour
     void Update()
     {
         ImGui.Begin("Steam Test Window");
-        ImGui.Text("User Steam Name: " + steamName);
+
+        ImGui.Text("Steam user name: " + steamName);
+        ImGui.Dummy(new Vector2(0.0f, 10.0f));
+
+        ImGui.Text("Available mods:");
+        foreach (Mod mod in ModManager.availableMods) {
+            ImGui.Text(mod.name);
+            ImGui.SameLine(120);
+            if (ImGui.Button("Upload to Steam Workshop")) {
+                // todo
+            }
+        }
+
         ImGui.End();
     }
 }
