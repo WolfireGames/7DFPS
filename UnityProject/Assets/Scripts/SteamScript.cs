9abeb6d7791dcdcc82b96f1b3029512304c64841
Tuomas Närväinen <tuomas.narvainen@alternativegames.net>
2020-05-27 16:31:16 +0300

    Refactor SteamScript a bit.

--- UnityProject/Assets/Scripts/SteamScript.cs
+++ UnityProject/Assets/Scripts/SteamScript.cs
@@ -19,18 +19,26 @@ public class SteamScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (optionsmenuscript.show_steam_ui) {
+            DrawSteamWindow();
+        }
+    }
+
+    void DrawSteamWindow() {
         ImGui.Begin("Steam Test Window");
 
         ImGui.Text("Steam user name: " + steamName);
         ImGui.Dummy(new Vector2(0.0f, 10.0f));
 
         ImGui.Text("Available mods:");
+        if (PlayerPrefs.GetInt("mods_enabled", 0) == 1) {
         foreach (Mod mod in ModManager.availableMods) {
             ImGui.Text(mod.name);
             ImGui.SameLine(120);
             if (ImGui.Button("Upload to Steam Workshop")) {
                 // todo
             }
+        }
         }
 
         ImGui.End();
