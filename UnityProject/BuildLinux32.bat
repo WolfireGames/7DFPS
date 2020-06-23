e64723a105463e04729fa0c33fed3e3e6ec77cf5
Max Danielsson <max@autious.net>
2020-06-24 00:40:49 +0200

    Upgrade unity build version to 2019_4_1

--- UnityProject/BuildLinux32.bat
+++ UnityProject/BuildLinux32.bat
@@ -1,7 +1,7 @@
 del /f /Q /S Receiver_linux32.zip
 del /f /Q /S "Build\linux32"
 
-"%UNITY_2019_4_0%" -quit -batchmode -projectPath "%~dp0" -executeMethod ReceiverBuildScript.BuildLinux32 -logFile unitylog.txt
+"%UNITY_2019_4_1%" -quit -batchmode -projectPath "%~dp0" -executeMethod ReceiverBuildScript.BuildLinux32 -logFile unitylog.txt
 
 if errorlevel 1 (
     echo Failure To Run
