0a238e12b18ad1ad38b57a0f5da8155ae9d2fe08
Max Danielsson <max@autious.net>
2020-06-24 00:40:49 +0200

    Upgrade unity build version to 2019_4_1

--- UnityProject/BuildMacOSX64.bat
+++ UnityProject/BuildMacOSX64.bat
@@ -1,7 +1,7 @@
 del /f /Q /S Receiver_macosx64.zip
 del /f /Q /S "Build\macosx64"
 
-"%UNITY_2019_4_0%" -quit -batchmode -projectPath "%~dp0" -executeMethod ReceiverBuildScript.BuildMacosx64 -logFile unitylog.txt
+"%UNITY_2019_4_1%" -quit -batchmode -projectPath "%~dp0" -executeMethod ReceiverBuildScript.BuildMacosx64 -logFile unitylog.txt
 
 if errorlevel 1 (
     echo Failure To Run
