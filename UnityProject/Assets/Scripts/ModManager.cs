7b26d8b869ad1afce64aa61f19e9e09afce80642
Kroporo <Michele.Butkus@gmx.de>
2020-08-18 15:35:35 +0200

    Changed the way we make sure SteamScript is initialized before ModManager.Awake is called

--- UnityProject/Assets/Scripts/ModManager.cs
+++ UnityProject/Assets/Scripts/ModManager.cs
@@ -35,7 +35,7 @@ public class ModManager : MonoBehaviour {
         }
     }
 
-    public void Start() {
+    public void Awake() {
         // Setup static reference
         ModManager._instance = this;
         steamScript = GameObject.FindObjectOfType<SteamScript>();
