d25476aa499fb8615251656086e349d2ef9a6c7d
Kroporo <Michele.Butkus@gmx.de>
2020-09-24 10:40:27 +0200

    Add Screen mode option

--- UnityProject/Assets/Scripts/GraphicsInitializer.cs
+++ UnityProject/Assets/Scripts/GraphicsInitializer.cs
@@ -13,7 +13,7 @@ public class GraphicsInitializer : MonoBehaviour {
 
     private static void ApplyResolution() {
         var resolution_id = PlayerPrefs.GetInt("resolution_setting", -1);
-        var fullscreen_mode = (FullScreenMode) PlayerPrefs.GetInt("fullscreen_setting", 0);
+        var fullscreen_mode = (FullScreenMode) PlayerPrefs.GetInt("screen_mode_setting", 0);
 
         if(Screen.resolutions.Length <= resolution_id || resolution_id < 0) { // Just select the highest one if we selected an invalid one
             resolution_id = Screen.resolutions.Length - 1;
@@ -21,7 +21,7 @@ public class GraphicsInitializer : MonoBehaviour {
 
         Resolution new_res = Screen.resolutions[resolution_id];
         Resolution current_res = Screen.currentResolution;
-        if(new_res.height != Screen.currentResolution.height || new_res.refreshRate != Screen.currentResolution.refreshRate || new_res.width != Screen.currentResolution.width) {
+        if(new_res.height != Screen.currentResolution.height || new_res.refreshRate != Screen.currentResolution.refreshRate || new_res.width != Screen.currentResolution.width || Screen.fullScreenMode != fullscreen_mode) {
             Screen.SetResolution(new_res.width, new_res.height, fullscreen_mode, new_res.refreshRate);
         }
     }
