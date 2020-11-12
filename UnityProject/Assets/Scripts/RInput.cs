1f1ba3807e31218a759905b59e7eb6e318aa5331
Kroporo <Michele.Butkus@gmx.de>
2020-11-12 16:04:20 +0100

    Properly serialize binding overrides

--- UnityProject/Assets/Scripts/RInput.cs
+++ UnityProject/Assets/Scripts/RInput.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using UnityEngine.InputSystem;
 using UnityEngine;
+using System.Linq;
 
 public class RInput : MonoBehaviour {
     public static MovementInputs player;
@@ -21,9 +22,6 @@ public class RInput : MonoBehaviour {
     public static void Init() {
         player = new MovementInputs();
         gun = new GunInputs();
-
-        LoadOverrides();
-        SaveOverrides();
         
         SetupInputActionMap(gun.main); // TODO can we do this in a loop somehow?
         SetupInputActionMap(player.main);
@@ -32,6 +30,9 @@ public class RInput : MonoBehaviour {
 
         gun.Enable();
         player.Enable();
+
+        LoadOverrides();
+        SaveOverrides();
 
         // Create an RInput object to hook into Unity's update cycle
         RInput instance = new GameObject().AddComponent<RInput>();
@@ -74,18 +75,84 @@ public class RInput : MonoBehaviour {
     }
 
     public static void SaveOverrides() {
-        PlayerPrefs.SetString("player_input", player.asset.ToJson());
-        PlayerPrefs.SetString("gun_input", gun.asset.ToJson());
+        PlayerPrefs.SetString("player_input", Save(player.asset).ToJSON());
+        PlayerPrefs.SetString("gun_input", Save(gun.asset).ToJSON());
     }
 
     public static void LoadOverrides() {
         if(PlayerPrefs.HasKey("player_input")) {
-            player.asset.LoadFromJson(PlayerPrefs.GetString("player_input"));
+            Load(player.asset, Overrides.FromJSON(PlayerPrefs.GetString("player_input")));
         }
 
         if(PlayerPrefs.HasKey("gun_input")) {
-            gun.asset.LoadFromJson(PlayerPrefs.GetString("gun_input"));
-            Debug.Log(PlayerPrefs.GetString("gun_input"));
+            Load(gun.asset, Overrides.FromJSON(PlayerPrefs.GetString("gun_input")));
+        }
+    }
+
+
+    // Serialization // TODO Replace with "SaveBindingOverridesAsJson" framework in InputSystem 1.1, once it comes out... And never look back
+
+    // Serializeable wrapper for action overrides
+    private class Overrides : Dictionary<System.Guid, string> {
+        private struct Serializeable {
+            public string[] keys;
+            public string[] values;
+        }
+
+        public static Overrides FromJSON(string json) {
+            Serializeable serializeable = JsonUtility.FromJson<Serializeable>(json);
+            
+            if(serializeable.keys == null || serializeable.values == null) {
+                return new Overrides();
+            }
+
+            if(serializeable.keys.Length != serializeable.values.Length) {
+                Debug.LogError("Corrupt keybind dictionary! Key and Value count don't match! Falling back to empty!");
+                return new Overrides();
+            }
+
+            Overrides overrides = new Overrides();
+            for (int i = 0; i < serializeable.keys.Length; i++) {
+                if(System.Guid.TryParse(serializeable.keys[i], out System.Guid guid)) {
+                    overrides.Add(guid, serializeable.values[i]);
+                } else {
+                    Debug.LogWarning($"Failed to parse guid: {serializeable.keys[i]}, mapped to {serializeable.values[i]}. Resetting to default!");
+                }
+            }
+            return overrides;
+        }
+
+        public string ToJSON() {
+            Serializeable serializeable = new Serializeable() {
+                keys = Keys.Select( (x) => x.ToString() ).ToArray(),
+                values = Values.ToArray(),
+            };
+            return JsonUtility.ToJson(serializeable);
+        }
+    }
+
+    // Modified from https://forum.unity.com/threads/saving-user-bindings.805722/#post-5384310
+    private static Overrides Save(InputActionAsset asset) {
+        Overrides overrides = new Overrides();
+        foreach (var map in asset.actionMaps) {
+            foreach (var binding in map.bindings) {
+                if (!string.IsNullOrEmpty(binding.overridePath)) {
+                    overrides[binding.id] = binding.overridePath;
+                }
+            }
+        }
+        return overrides;
+    }
+
+    // Modified from https://forum.unity.com/threads/saving-user-bindings.805722/#post-5384310 
+    private static void Load(InputActionAsset asset, Overrides overrides) {
+        foreach (var map in asset.actionMaps) {
+            var bindings = map.bindings;
+            for (var i = 0; i < bindings.Count; ++i) {
+                if (overrides.TryGetValue(bindings[i].id, out var overridePath)) {
+                    map.ApplyBindingOverride(i, new InputBinding { overridePath = overridePath });
+                }
+            }
         }
     }
 }
