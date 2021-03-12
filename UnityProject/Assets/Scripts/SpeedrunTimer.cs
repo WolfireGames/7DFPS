524433938305c82548ada5f024a7c7972a16083b
Kroporo <Michele.Butkus@gmx.de>
2021-03-12 14:27:46 +0100

    Implement simple split information when picking up tapes

--- UnityProject/Assets/Scripts/SpeedrunTimer.cs
+++ UnityProject/Assets/Scripts/SpeedrunTimer.cs
@@ -9,6 +9,13 @@ public class SpeedrunTimer : MonoBehaviour {
 
     public Color runningColor = Color.white;
     public Color lockedColor = Color.blue;
+    public Color noticeColor = Color.green;
+
+    private int currentSplit = 0;
+    private float splitTime = 0;
+    private float splitDuration = 0f;
+    private float splitOpacity = 0f;
+    private string splitText = "";
 
     void Start() {
         style = GameObject.Find("gui_skin_holder").GetComponent<GUISkinHolder>().gui_skin.label;
@@ -18,13 +25,26 @@ public class SpeedrunTimer : MonoBehaviour {
         StartTimer();
     }
 
+    private void Update() {
+        if(splitDuration > 0) {
+            splitOpacity = Mathf.Clamp01(splitOpacity + Time.deltaTime * 4);
+            splitDuration -= Time.deltaTime;
+        } else {
+            splitOpacity = Mathf.Clamp01(splitOpacity - Time.deltaTime);
+        }
+    }
+
     public string GetTimeString() {
         float time = Time.time;
         if(isLocked) { // Override with locked in time
             time = endTime;
         }
 
-        return TimeSpan.FromSeconds(time - startTime).ToString(@"mm\:ss\:fff");
+        return FormatDurationString(time - startTime);
+    }
+
+    public string FormatDurationString(float duration) {
+        return TimeSpan.FromSeconds(duration).ToString(@"mm\:ss\:fff");
     }
 
     public void StopTimer() {
@@ -41,6 +61,13 @@ public class SpeedrunTimer : MonoBehaviour {
         isLocked = false;
     }
 
+    public void Split() {
+        currentSplit++;
+        splitDuration = 5f;
+        splitText = $"[{currentSplit}] {FormatDurationString(Time.time - splitTime)}";
+        splitTime = Time.time;
+    }
+
     public void OnGUI() {
         if(PlayerPrefs.GetInt("speedrun_timer", 0) != 1) {
             return;
@@ -53,5 +80,23 @@ public class SpeedrunTimer : MonoBehaviour {
         
         style.normal.textColor = isLocked ? lockedColor : runningColor;
         GUI.Label(new Rect(width - 4.5f, Screen.height - 25f, width, 30f), GetTimeString(), style);
+
+        DrawSplitGUI();
+    }
+
+    private void DrawSplitGUI() {
+        if(splitOpacity > 0) {
+            float width = Screen.width * 0.5f;
+            int prevFontSize = style.fontSize;
+
+            style.fontSize = 12;
+            style.normal.textColor = new Color(0f, 0f, 0f, splitOpacity);
+            GUI.Label(new Rect(width - 4f, Screen.height - 40f, width + 0.5f, 20 + 0.5f), splitText, style);
+
+            noticeColor.a = splitOpacity;
+            style.normal.textColor = noticeColor;
+            GUI.Label(new Rect(width - 4.5f, Screen.height - 40f, width, 30f), splitText, style);
+            style.fontSize = prevFontSize;
+        }
     }
 }
