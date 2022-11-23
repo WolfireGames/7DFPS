cd68bd2c31fa2c358b0f126064ac4acc82713379
Kroporo <michele.butkus@octacon.se>
2022-11-23 09:47:40 +0100

    Update unity version to 2021.3.13f1
    I've recently removed 2019.29f1 from the build server, so now these builds are failing.
    Could just reinstall it, but this seems like a better option

--- UnityProject/BuildLinux32.bat
+++ UnityProject/BuildLinux32.bat
@@ -1,7 +1,7 @@
 del /f /Q /S Receiver_linux32.zip
 del /f /Q /S "Build\linux32"
 
-"%UNITY_HUB%/Editor/2019.4.29f1/Editor/Unity.exe" -quit -batchmode -projectPath "%~dp0" -executeMethod ReceiverBuildScript.BuildLinux32 -logFile unitylog.txt
+"%UNITY_HUB%/Editor/2021.3.13f1/Editor/Unity.exe" -quit -batchmode -projectPath "%~dp0" -executeMethod ReceiverBuildScript.BuildLinux32 -logFile unitylog.txt
 
 if errorlevel 1 (
     echo Failure To Run
--- UnityProject/BuildMacOSX64.bat
+++ UnityProject/BuildMacOSX64.bat
@@ -1,7 +1,7 @@
 del /f /Q /S Receiver_macosx64.zip
 del /f /Q /S "Build\macosx64"
 
-"%UNITY_HUB%/Editor/2019.4.29f1/Editor/Unity.exe" -quit -batchmode -projectPath "%~dp0" -executeMethod ReceiverBuildScript.BuildMacosx64 -logFile unitylog.txt
+"%UNITY_HUB%/Editor/2021.3.13f1/Editor/Unity.exe" -quit -batchmode -projectPath "%~dp0" -executeMethod ReceiverBuildScript.BuildMacosx64 -logFile unitylog.txt
 
 if errorlevel 1 (
     echo Failure To Run
