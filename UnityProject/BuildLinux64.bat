29fddcde227c027862ede33eacc3f88420602709
Kroporo <michele.butkus@octacon.se>
2022-12-20 13:09:24 +0100

    Update Unity to 2021.3.16f1

--- UnityProject/BuildLinux64.bat
+++ UnityProject/BuildLinux64.bat
@@ -1,7 +1,7 @@
 del /f /Q /S Receiver_linux64.zip
 del /f /Q /S "Build\linux64"
 
-"%UNITY_HUB%/Editor/2021.3.13f1/Editor/Unity.exe" -quit -batchmode -projectPath "%~dp0" -executeMethod ReceiverBuildScript.BuildLinux64 -logFile unitylog.txt
+"%UNITY_HUB%/Editor/2021.3.16f1/Editor/Unity.exe" -quit -batchmode -projectPath "%~dp0" -executeMethod ReceiverBuildScript.BuildLinux64 -logFile unitylog.txt
 
 if errorlevel 1 (
     echo Failure To Run
