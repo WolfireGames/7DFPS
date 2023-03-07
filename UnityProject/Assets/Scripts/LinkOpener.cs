5a9f868089affb62f9ac619293a9b1dcb1f8d2c1
Max Danielsson <max@autious.net>
2023-03-07 11:37:42 +0100

    Add receiver 2 ad in pause menu

---
+++ UnityProject/Assets/Scripts/LinkOpener.cs
@@ -0,0 +1,15 @@
+using Steamworks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkOpener : MonoBehaviour {
+    public void OpenLink() {
+        if(SteamAPI.IsSteamRunning()) {
+            SteamFriends.ActivateGameOverlayToStore(new Steamworks.AppId_t(1129310), EOverlayToStoreFlag.k_EOverlayToStoreFlag_None);
+        } else {
+            Application.OpenURL("https://store.steampowered.com/app/1129310/Receiver_2/");
+        }
+
+    }
+}
